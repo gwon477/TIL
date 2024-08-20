cc45a614baa16d59275ac2d9ad2878507b608bfa
gwonsang <gwonsang247@gmail.com>
2024-08-20 11:38:21 +0900

    오픈 채팅방

--- 유형별 문제 풀이/그리디/요격시스템.py
+++ 유형별 문제 풀이/그리디/요격시스템.py
@@ -4,7 +4,7 @@ def solution(targets):
     targets = sorted(targets, key=lambda x: x[1])
     
     rocket_location = float('-inf')
-    #print(targets)
+
     for i in range(0,N):
         if targets[i][0] >= rocket_location:
             answer += 1
